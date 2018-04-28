810658fcff63fe481399ef06200d2f6d9304a93c
programacion <programacion@wilson-EURO0>
2018-04-28 10:07:10 -0300

    dasd

--- TP_2018/src/data_access_layer/TaskCollectionDAOImpl.java
+++ TP_2018/src/data_access_layer/TaskCollectionDAOImpl.java
@@ -1,24 +1,45 @@
 package data_access_layer;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import bussines_layer.Task;
 
 public class TaskCollectionDAOImpl implements DAOTask {
 		
-		List<Task>taskList;
+		List<Task>taskList = new ArrayList<Task>();
 
 		public void register(Task task) {
+			taskList.clear();
 			
 			if(exists(task)==false) {
 				taskList.add(task);
+				
 			}
 			else {
 				System.out.println("esa tarea ya existe");
+				
 			}
 			
 		}
+		
+		public void synchronize() {
+			Synchronize.saveTask(taskList);
+		}
+		public void imprimirTask() {
+			try {
+				Synchronize.printTask();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		public void leerTexto() {
+			Synchronize.leertext();
 
+		}
+		
 		public void modify(Task task) {
 			// TODO Auto-generated method stub
 			
